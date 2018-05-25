eb080cd110b6ce24f75ab3a1d11fb098454551fd
Ana Correia <sorcha08@hotmail.com>
2018-05-25 12:35:00 +0100

    new fonts

---
+++ js/mailchimp_popup.js
@@ -0,0 +1,10 @@
+<script type="text/javascript" src="https://downloads.mailchimp.com/js/signup-forms/popup/embed.js" data-dojo-config="usePlainJson: true, isDebug: false"></script>
+
+
+<script>
+function showMailingPopUp() {
+    require(["mojo/signup-forms/Loader"], function(L) { L.start({"baseUrl":"mc.us8.list-manage.com","uuid":"e701d26deb60c6b77a91b6f09","lid":"347f53988d"});
+};
+
+document.getElementById("open-popup").onclick = function() {showMailingPopUp()};
+</script>
